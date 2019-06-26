9b0b8de11dfa4985fdbb80977774a84f796c8a43
josernesto <jecm3>
2019-06-26 16:24:11 -0400

    commit con loginpage verificado

--- Tarea3/Tarea3/Tarea3/App.xaml.cs
+++ Tarea3/Tarea3/Tarea3/App.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using Tarea3.Views;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -11,7 +12,7 @@ namespace Tarea3
         {
             InitializeComponent();
 
-            MainPage = new MainPage();
+            MainPage = new LoginPage();
         }
 
         protected override void OnStart()
